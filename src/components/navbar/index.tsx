d2bf1819ae38d92b0970b79f0643c69bedd910d0
Neil Ji <liubaimail@163.com>
2024-01-20 16:01:39 +0800

    finish nav component

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,19 +1,35 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./index.module.css";
 
+const tabs = ["", "tags", "archives"];
+// TODO: I18N
+const textMap: { [key: string]: string } = {
+  home: "首页",
+  tags: "标签",
+  archives: "归档",
+};
+
 export function Navbar() {
+  const path = usePathname();
+
   return (
     <nav className={styles.root}>
       <div role={"list"}>
-        <div role={"listitem"}>
-          <Link href="./">首页</Link>
-        </div>
-        <div role={"listitem"}>
-          <Link href="./tags">标签</Link>
-        </div>
-        <div role={"listitem"}>
-          <Link href="./archives">归档</Link>
-        </div>
+        {tabs.map((value) => {
+          const activeItemClassName =
+            path === `/${value}` ? styles.listItemActive : "";
+          return (
+            <div
+              key={value}
+              role={"listitem"}
+              className={`${styles.listItem} ${activeItemClassName}`}
+            >
+              <Link href={`./${value}`}>{textMap[value || "home"]}</Link>
+            </div>
+          );
+        })}
       </div>
     </nav>
   );
